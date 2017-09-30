9e48cdb73f7faa2c1d50dfe5be4bf36f41b9ab63
Steve Jewkes <steve@mulus.net>
2017-09-30 09:49:18 +0100

    An attempt to tidy up the code by adding a coordinates class

--- mazegen.py
+++ mazegen.py
@@ -2,7 +2,35 @@
 import sys
 import argparse
 import random
+import traceback
 from PIL import Image
+
+class Coord:
+    def __init__(self, x=0, y=0):
+        self.coord = (x, y)
+
+    def __str__(self):
+        return "(x={}, y={})".format(self.coord[0], self.coord[1])
+
+    def __add__(self, other):
+        if isinstance(other, tuple) and len(other) is 2:
+            return Coord(self.coord[0] + other[0], self.coord[1] + other[1])
+        elif isinstance(other, Coord):
+            return self.__add__(other.coord)
+        else:
+            raise ValueError("A list of length 2 is required")
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def x(self):
+        return self.coord[0]
+
+    def y(self):
+        return self.coord[1]
+
+    def conv_1d(self, width):
+        return self.y() * width + self.x()
 
 class MazeGen:
     NORTH = 0x1
@@ -17,40 +45,40 @@ class MazeGen:
         self.maze = [0] * (width * height)
 
         self.visited = []
-        self.visited.append((0,0))
+        self.visited.append(Coord(0,0))
         self.visited_count = width * height
-        self.add_dir(self.visited[-1], MazeGen.VISITED)
+        self.update_state(self.visited[-1], MazeGen.VISITED)
 
     def __str__(self):
         str = "Width: {}\nHeight: {}\n".format(self.width, self.height)
         for y in range(self.height):
             for x in range(self.width):
-                str += "{:02x} ".format(self.get((x,y)))
+                str += "{:02x} ".format(self.get(Coord(x,y)))
             str += "\n"
         return str
 
-    def get(self, coords):
-        return self.maze[coords[1] * self.width + coords[0]]
+    def get(self, coord):
+        return self.maze[coord.conv_1d(self.width)]
 
-    def add_dir(self, coords, dir):
-        self.maze[coords[1] * self.width + coords[0]] |= dir
+    def update_state(self, coord, state):
+        self.maze[coord.conv_1d(self.width)] |= state
 
-    def get_free_exits(self, coords):
+    def get_free_exits(self, coord):
         tmp_exits = []
-        state = self.get(coords)
+        state = self.get(coord)
 
-        if state & MazeGen.NORTH is 0 and coords[1] > 0:
-            tmp_exits.append((MazeGen.NORTH, MazeGen.SOUTH, 0, -1))
-        if state & MazeGen.SOUTH is 0 and coords[1] < (self.height - 1):
-            tmp_exits.append((MazeGen.SOUTH, MazeGen.NORTH, 0, 1))
-        if state & MazeGen.WEST is 0 and coords[0] > 0:
-            tmp_exits.append((MazeGen.WEST, MazeGen.EAST, -1, 0))
-        if state & MazeGen.EAST is 0 and coords[0] < (self.width - 1):
-            tmp_exits.append((MazeGen.EAST, MazeGen.WEST, 1, 0))
+        if state & MazeGen.NORTH is 0 and coord.y() > 0:
+            tmp_exits.append((MazeGen.NORTH, MazeGen.SOUTH, Coord(0, -1)))
+        if state & MazeGen.SOUTH is 0 and coord.y() < (self.height - 1):
+            tmp_exits.append((MazeGen.SOUTH, MazeGen.NORTH, Coord(0, 1)))
+        if state & MazeGen.WEST is 0 and coord.x() > 0:
+            tmp_exits.append((MazeGen.WEST, MazeGen.EAST, Coord(-1, 0)))
+        if state & MazeGen.EAST is 0 and coord.x() < (self.width - 1):
+            tmp_exits.append((MazeGen.EAST, MazeGen.WEST, Coord(1, 0)))
 
         exits = []
         for exit in tmp_exits:
-            if self.get((coords[0] + exit[2], coords[1] + exit[3])) & MazeGen.VISITED is 0:
+            if self.get(coord + exit[2]) & MazeGen.VISITED is 0:
                 exits.append(exit)
 
         return exits
@@ -59,22 +87,22 @@ class MazeGen:
         # Build maze whilst there are still unvisited cells
         while self.visited and self.visited_count > 0:
             # Get available exits from the currently visited cell
-            curr_coords = self.visited[-1]
-            exits = self.get_free_exits(curr_coords)
+            curr_coord = self.visited[-1]
+            exits = self.get_free_exits(curr_coord)
             if exits:
                 # If there are still untried exits then randomly choose one and
                 # add it to top of currently visited cells
                 choice = random.choice(exits)
-                self.add_dir(curr_coords, choice[0])
+                self.update_state(curr_coord, choice[0])
 
-                choice_coords = (curr_coords[0] + choice[2], curr_coords[1] + choice[3])
-                self.visited.append(choice_coords)
-                self.add_dir(self.visited[-1], choice[1])
+                choice_coord = curr_coord + choice[2]
+                self.visited.append(choice_coord)
+                self.update_state(self.visited[-1], choice[1])
 
-                if self.get(choice_coords) & MazeGen.VISITED is 0:
+                if self.get(choice_coord) & MazeGen.VISITED is 0:
                     # If this new cell has not been visited before then
                     # mark it as such
-                    self.add_dir(choice_coords, MazeGen.VISITED)
+                    self.update_state(choice_coord, MazeGen.VISITED)
                     self.visited_count -= 1
             else:
                 # No available exits, so don't visit this cell in future
@@ -84,8 +112,7 @@ class MazeGen:
         image = Image.new('RGB', (self.width * 2, self.height *2), (0,0,0))
         for y in range(self.height):
             for x in range(self.width):
-                coords = (x,y)
-                state = self.get(coords)
+                state = self.get(Coord(x, y))
                 if state & MazeGen.EAST is 0:
                     image.putpixel(((x*2) + 1, y*2), (255, 255, 255))
                 if state & MazeGen.SOUTH is 0:
@@ -116,6 +143,8 @@ def main():
         maze.render(args.cellw, args.cellh)
 
     except Exception as e:
+        if args.verbose:
+            traceback.print_exc()
         print("Failed with error:\n{}".format(e))
 
 if __name__ == "__main__":
